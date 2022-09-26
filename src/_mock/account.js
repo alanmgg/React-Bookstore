56c0f53bac1868faea96d453889c09968d25de8e
alanmgg <alanfmorag@gmail.com>
2022-09-25 20:28:57 -0500

    :up: Initial commit

---
+++ src/_mock/account.js
@@ -0,0 +1,10 @@
+// ----------------------------------------------------------------------
+
+const account = {
+  displayName: 'Celeste Herrera',
+  email: 'dayanceles@books.com',
+  role: 'Frontend Developer',
+  photoURL: '/static/mock-images/avatars/avatar_21.jpg',
+};
+
+export default account;
