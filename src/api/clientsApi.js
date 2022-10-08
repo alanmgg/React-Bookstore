af219dbebbb3277d580c6af660c32831d8ac67aa
alanmgg <alanfmorag@gmail.com>
2022-10-07 21:11:15 -0500

    :recycle: Prueba API

--- src/api/clientsApi.js
+++ src/api/clientsApi.js
@@ -8,11 +8,13 @@ export function getClients(responseHandler = handleResponse, errorHandler = hand
 }
 
 export function getClienteByEmail(email, password, responseHandler = handleResponse, errorHandler = handleError) {
-    let endpoint_url = "http://44.204.17.142/clientes/" + email + "/" + password
-    return fetch(endpoint_url)
-        .then(responseHandler, {
-            headers: { "Content-Security-Policy": "block-all-mixed-content" }
-        })
+    let endpoint_url = "https://bookbay.duckdns.org/api/v1/clientes/" + email + "/" + password
+    // let endpoint_url = "http://44.204.17.142/clientes/" + email + "/" + password
+    return fetch(endpoint_url, {
+        headers: { "Access-Control-Allow-Origin": "*" },
+        redirect: 'follow'
+    })
+        .then(responseHandler)
         .catch(errorHandler);
 }
 
